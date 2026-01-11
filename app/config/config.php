a5b04bf608ef49812ec7d13aa27ee135016337a9
Juan David <jdavidreyes09@gmail.com>
2026-01-11 08:13:21 -0500

    v2.1

--- app/config/config.php
+++ app/config/config.php
@@ -3,22 +3,17 @@
 declare(strict_types=1);
 
 // Base URL path (para que los assets/links funcionen si el proyecto no está en la raíz del dominio).
-// TEMPORAL: Forzar /test/ para producción
-// TODO: Implementar detección automática después de debug
-if (isset($_SERVER['HTTP_HOST']) && str_contains($_SERVER['HTTP_HOST'], 'test.codigonativo.com')) {
-    define('URL_PATH', '/test/');
-} else {
-    // Para desarrollo local
-    $scriptName = $_SERVER['SCRIPT_NAME'] ?? '/index.php';
-    $scriptDir = str_replace('\\', '/', dirname($scriptName));
-    
-    if (str_ends_with($scriptDir, '/public')) {
-        $scriptDir = dirname($scriptDir);
-    }
-    
-    $scriptDir = $scriptDir === '/' ? '/' : rtrim($scriptDir, '/') . '/';
-    define('URL_PATH', $scriptDir);
+$scriptName = $_SERVER['SCRIPT_NAME'] ?? '/index.php';
+$scriptDir = str_replace('\\', '/', dirname($scriptName));
+
+// Si estamos en /public/ (cuando se accede por .htaccess que redirige a public/)
+// ajustar para que URL_PATH sea /
+if (str_ends_with($scriptDir, '/public')) {
+    $scriptDir = dirname($scriptDir);
 }
 
+$scriptDir = $scriptDir === '/' ? '/' : rtrim($scriptDir, '/') . '/';
+
+define('URL_PATH', $scriptDir);
 define('APP_PATH', dirname(__DIR__));
 define('VIEW_PATH', APP_PATH . '/Views');
